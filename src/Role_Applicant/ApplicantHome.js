33061639d8b524bed51f7cb8f0ace763f6ced3c2
Solomon141 <weymit2001@gmail.com>
2024-03-16 16:34:08 +0300

    Applicant Reviewing

--- src/Role_Applicant/ApplicantHome.js
+++ src/Role_Applicant/ApplicantHome.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import ListGroup from "react-bootstrap/ListGroup";
+import Button from "react-bootstrap/Button"
 
 function ApplicantHome() {
+  const [externalId, setExternalId] = useState(0);
+
+  const getClientByExternalId = () => {}
+
   return (
-    <div>ApplicantHome</div>
-  )
+    <div>
+      <ToastContainer />
+      <div className="row">
+        <div className="col-sm-12">
+          <InputGroup className="mb-3">
+            <Form.Control
+              placeholder="Client External ID"
+              aria-label="Client External ID"
+              aria-describedby="basic-addon2"
+              onChange={(ev) => {
+                setExternalId(ev.target.value);
+              }}
+            />
+            <Button
+              onClick={getClientByExternalId}
+              variant="outline-secondary"
+              id="button-addon2"
+            >
+              Search
+            </Button>
+          </InputGroup>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ApplicantHome
+export default ApplicantHome;
---
+++ src/Role_Applicant/CustomerEdit.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CustomerEdit() {
+  return (
+    <div>CustomerEdit</div>
+  )
+}
+
+export default CustomerEdit
